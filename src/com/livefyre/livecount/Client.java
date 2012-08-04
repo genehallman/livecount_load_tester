7d0360408c6f16d4e53da36443cf808bcbbdbc23
Gene Hallman <gene@livefyre.com>
2012-08-04 12:09:58 -0700

    awesomeness

--- src/com/livefyre/livecount/Client.java
+++ src/com/livefyre/livecount/Client.java
@@ -14,107 +14,124 @@ import com.livefyre.livecount.PerformanceMetric.RequestType;
 
 public class Client implements Runnable {
 
-	private String host;
-	private int convId;
-	private int userId;
-	private String routedHost = null;
-	private boolean timeout = false;
+    private String host;
+    private int convId;
+    private int userId;
+    private String routedHost = null;
+    private boolean timeout = false;
 
-	public Client(String host, int convId, int userId) {
-		this.host = host;
-		this.convId = convId;
-		this.userId = userId;
-	}
+    public Client(String host, int convId, int userId) {
+        this.host = host;
+        this.convId = convId;
+        this.userId = userId;
+    }
 
-	private JSONObject makeRequest() throws Exception {
-		HttpURLConnection connection = null;
-		try {
-			String url = String.format("%s/livecountping/%d/%d/",
-					this.routedHost == null ? this.host : this.routedHost, this.convId, this.userId);
+    private JSONObject makeRequest() throws Exception {
+        HttpURLConnection connection = null;
+        try {
+            String url = String.format("%s/livecountping/%d/%d/", this.routedHost == null ? this.host : this.routedHost, this.convId,
+                    this.userId);
+            String params = "?";
+            if (timeout) {
+                params += "timeout=1";
+            } else if (routedHost != null) {
+                params += "routed=1";
+            }
+            if (params.length() > 1) {
+                params += "&";
+            }
+            params += "callback=asdf";
 
-			if (timeout) {
-				url += "?timeout=1";
-			} else if (routedHost != null) {
-				url += "?routed=1";
-			}
+            URL serverAddress = new URL(url + params);
+            connection = (HttpURLConnection) serverAddress.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setReadTimeout(3000);
+            connection.connect();
 
-			URL serverAddress = new URL(url);
-			connection = (HttpURLConnection) serverAddress.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setDoOutput(true);
-			connection.setReadTimeout(3000);
-			connection.connect();
+            BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = response.readLine()) != null) {
+                sb.append(line + '\n');
+            }
 
-			BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while ((line = response.readLine()) != null) {
-				sb.append(line + '\n');
-			}
+            String rawResp = sb.toString().trim();
 
-			JSONObject resp = new JSONObject((sb.toString()));
+            if (rawResp.startsWith("asdf(")) {
+                rawResp = rawResp.replaceFirst("asdf\\(", "");
+            }
 
-			// p(resp);
-			return resp;
+            if (rawResp.endsWith(");")) {
+                System.out.print("0");
+                rawResp = rawResp.replaceFirst("\\);$", "");
+            } else {
+                System.out.print("1");
+            }
 
-		} catch (SocketTimeoutException e) {
-			timeout = true;
-			routedHost = null;
-			throw e;
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			// close the connection, set all objects to null
-			if (connection != null) {
-				connection.disconnect();
-			}
-		}
-	}
+            JSONObject resp = new JSONObject(rawResp);
 
-	private void processResponse(JSONObject resp, PerformanceMetric metric) throws JSONException {
-		timeout = false;
-		int code = resp.getInt("code");
-		switch (code) {
-		case 200:
-			break;
-		case 302:
-			routedHost = resp.getString("data");
-			break;
-		case 500:
-			routedHost = null;
-			break;
-		}
-	}
+            // p(resp);
+            return resp;
 
-	public void run() {
-		StopWatch timer = null;
-		PerformanceMetric metric = null;
-		try {
-			timer = new StopWatch();
-			metric = new PerformanceMetric();
-			metric.requestType = RequestType.Count;
-			if (timeout) {
-				metric.requestType = RequestType.Timeout;
-			} else if (routedHost == null) {
-				metric.requestType = RequestType.Route;
-			}
-			timer.start();
+        } catch (SocketTimeoutException e) {
+            timeout = true;
+            routedHost = null;
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // close the connection, set all objects to null
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
 
-			JSONObject resp = makeRequest();
-			metric.roundTripTime = timer.getElapsedTime();
-			timer.stop();
+    private void processResponse(JSONObject resp, PerformanceMetric metric) throws JSONException {
+        timeout = false;
+        int code = resp.getInt("code");
+        switch (code) {
+        case 200:
+            break;
+        case 302:
+            routedHost = resp.getString("data");
+            break;
+        case 500:
+            routedHost = null;
+            break;
+        }
+    }
 
-			metric.returnCode = resp.getInt("code");
-			processResponse(resp, metric);
+    public void run() {
+        StopWatch timer = null;
+        PerformanceMetric metric = null;
+        try {
+            timer = new StopWatch();
+            metric = new PerformanceMetric();
+            metric.requestType = RequestType.Count;
+            if (timeout) {
+                metric.requestType = RequestType.Timeout;
+            } else if (routedHost == null) {
+                metric.requestType = RequestType.Route;
+            }
+            timer.start();
 
-		} catch (Exception e) {
-			if (metric != null) {
-				metric.exception = e;
-				metric.roundTripTime = metric.roundTripTime > 0 ? metric.roundTripTime : timer.getElapsedTime();
-				timer.stop();
-			}
-		} finally {
-			LoadTester.metrics.add(metric);
-		}
-	}
+            JSONObject resp = makeRequest();
+            metric.roundTripTime = timer.getElapsedTime();
+            timer.stop();
+
+            metric.returnCode = resp.getInt("code");
+            processResponse(resp, metric);
+
+        } catch (Exception e) {
+            if (metric != null) {
+                metric.exception = e;
+                metric.roundTripTime = metric.roundTripTime > 0 ? metric.roundTripTime : timer.getElapsedTime();
+                timer.stop();
+            }
+        } finally {
+            LoadTester.metrics.add(metric);
+        }
+    }
 }
--- src/com/livefyre/livecount/LoadTester.java
+++ src/com/livefyre/livecount/LoadTester.java
@@ -6,101 +6,98 @@ import static com.livefyre.livecount.Util.randomId;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class LoadTester implements Runnable {
-	public static MetricAggregator metrics = new MetricAggregator();
+    public static MetricAggregator metrics = new MetricAggregator();
 
-	private int nThreads = 8;
-	private int nRequests = 8000;
-	private List<String> hosts;
-	private int delay = 5;
+    private int nThreads = 600;
+    private int nRequests = 6000;
+    private List<String> hosts;
+    private int delay = 5;
 
-	public static Builder newbuilder() {
-		return new Builder();
-	}
+    public static Builder newbuilder() {
+        return new Builder();
+    }
 
-	private LoadTester() {
-		hosts = new ArrayList<String>();
-		hosts.add("http://genes-macbook-pro.local:8905");
-		hosts.add("http://genes-macbook-pro.local:8906");
-		hosts.add("http://genes-macbook-pro.local:8907");
-	}
+    private LoadTester() {
+        hosts = new ArrayList<String>();
+        hosts.add("http://genes-macbook-pro.local:8905");
+        // hosts.add("http://genes-macbook-pro.local:8906");
+        // hosts.add("http://genes-macbook-pro.local:8907");
+    }
 
-	public void run() {
-		p("Thread %s starting %d clients", Thread.currentThread().getName(), (int) Math.floor(nRequests / nThreads));
-		Client[] clients = new Client[(int) Math.floor(nRequests / nThreads)];
+    public void run() {
+        p("Thread %s starting %d clients", Thread.currentThread().getName(), (int) Math.floor(nRequests / nThreads));
+        Client[] clients = new Client[(int) Math.floor(nRequests / nThreads)];
 
-		for (int i = 0; i < clients.length; i++) {
-			clients[i] = new Client(randomHost(hosts), randomId(1), randomId(10000000));
-		}
-		int i = 0;
-		while (true) {
-			clients[i].run();
-			i++;
-			if (i >= clients.length) {
-				i = 0;
-			}
+        for (int i = 0; i < clients.length; i++) {
+            clients[i] = new Client(randomHost(hosts), randomId(10000000), randomId(1000000000));
+        }
+        int i = 0;
+        while (true) {
+            clients[i].run();
+            i++;
+            if (i >= clients.length) {
+                i = 0;
+            }
 
-			try {
-				Thread.sleep(delay);
-			} catch (InterruptedException e) {
-			}
-		}
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+            }
+        }
 
-	}
+    }
 
-	public void start() throws InterruptedException {
-		p("Starting Load Tester { threads: %d, requests: %d }", nThreads, (int) Math.floor(nRequests / nThreads)
-				* nThreads);
+    public void start() throws InterruptedException {
+        p("Starting Load Tester { threads: %d, requests: %d }", nThreads, (int) Math.floor(nRequests / nThreads) * nThreads);
 
-		for (int i = 0; i < nThreads; i++) {
-			new Thread(this).start();
-		}
-		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
-			public void run() {
-				p(metrics);
-			}
-		}, 0, 10, TimeUnit.SECONDS);
-	}
+        for (int i = 0; i < nThreads; i++) {
+            new Thread(this).start();
+        }
+        /*
+         * Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new
+         * Runnable() { public void run() { p(metrics); } }, 0, 10,
+         * TimeUnit.SECONDS);
+         */
+    }
 
-	static class Builder {
-		private boolean isBuilt = false;
-		private LoadTester loadTester;
+    static class Builder {
+        private boolean isBuilt = false;
+        private LoadTester loadTester;
 
-		public Builder() {
-			loadTester = new LoadTester();
-		}
+        public Builder() {
+            loadTester = new LoadTester();
+        }
 
-		private void checkBuilt() {
-			if (isBuilt) {
-				throw new IllegalStateException("The object cannot be modified after built");
-			}
-		}
+        private void checkBuilt() {
+            if (isBuilt) {
+                throw new IllegalStateException("The object cannot be modified after built");
+            }
+        }
 
-		public Builder setThreads(int val) {
-			checkBuilt();
-			loadTester.nThreads = val;
-			return this;
-		}
+        public Builder setThreads(int val) {
+            checkBuilt();
+            loadTester.nThreads = val;
+            return this;
+        }
 
-		public Builder setHosts(List<String> val) {
-			checkBuilt();
-			loadTester.hosts = val;
-			return this;
-		}
+        public Builder setHosts(List<String> val) {
+            checkBuilt();
+            loadTester.hosts = val;
+            return this;
+        }
 
-		public Builder setRequests(int val) {
-			checkBuilt();
-			loadTester.nRequests = val;
-			return this;
-		}
+        public Builder setRequests(int val) {
+            checkBuilt();
+            loadTester.nRequests = val;
+            return this;
+        }
 
-		public LoadTester build() {
-			checkBuilt();
-			isBuilt = true;
-			return loadTester;
-		}
-	}
+        public LoadTester build() {
+            checkBuilt();
+            isBuilt = true;
+            return loadTester;
+        }
+    }
 }
